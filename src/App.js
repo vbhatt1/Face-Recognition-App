40cf9239fe1e91de9f52e14bd958b111e556fb6d
VIHAR\vihar <viharbhatt95@yahoo.in>
2018-08-05 03:54:30 -0700

    Added the components for the app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+
+const particlesParam = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+    }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles"
+              params={particlesParam}
+            />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+
+      </div>
+    );
+  }
+}
+
+export default App;
