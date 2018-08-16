457b1d3c3dcafce8e1f0f478233e31017878cb5f
VIHAR\vihar <viharbhatt95@yahoo.in>
2018-08-16 01:04:27 -0700

    Updated the database.

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'localhost',
-		user: 'postgres',
-		password: 'viharbhatt',
-		database: 'face-recognition'	
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
